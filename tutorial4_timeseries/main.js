5027760df3637cfe5cbed3f648dab505192b0a9a
lauralonglastname <lauramuellersoppart@gmail.com>
2021-03-05 23:58:28 -0500

    working on svg

--- tutorial4_timeseries/main.js
+++ tutorial4_timeseries/main.js
@@ -11,7 +11,7 @@ let xScale;
 let yScale;
 
 /* APPLICATION STATE */
-let state = {
+let state = { 
   data: [],
   selection: "Federal", // + YOUR FILTER SELECTION
 };
@@ -19,78 +19,115 @@ let state = {
 /* LOAD DATA */
 // + SET YOUR DATA PATH
 d3.csv('../data/MinWageData2020Dollars.csv', (d)=> {
-  console.log(d)
 return {
   usstate: d.State,
   minwage: +d.StateMinWage,
   fedwage: +d.FedMinWage,
   year: new Date(d.Year, 01, 01) 
 }
-})
+}) 
   .then(data => {
     console.log("loaded data:", data);
     state.data = data;
-    init();
+    init(); 
   });
 
-/* INITIALIZING FUNCTION */
-// this will be run *one time* when the data finishes loading in
+// /* INITIALIZING FUNCTION */
+// // this will be run *one time* when the data finishes loading in
 function init() {
-  // SCALES
+//   // + DEFINE SCALES
   xScale = d3.scaleTime()
-  .domain(d3.extent(state.data, d=> d.year))
+  .domain(d3.extent(state.data, d=>d.year))
   .range([margin.left, width - margin.right])
-
-yScale = d3.scaleLinear()
-  .domain(d3.extent(state.data, d=> d.envScore2020)) // [min, max]
-  .range([height-margin.bottom, margin.top])
-
-// AXES
-const xAxis = d3.axisBottom(xScale)
-const yAxis = d3.axisLeft(yScale)
-
-// Create svg
+  
+  yScale = d3.scaleLinear()
+  .domain(d3.extent(state.data, d=> d.minwage))
+  .range([height - margin.bottom, margin.bottom])
+  
+//     // + DEFINE AXES 
+    const xAxis = d3.axisBottom(xScale)
+    const yAxis = d3.axisLeft(yScale) 
+  
+//     //Create svg
+// + CREATE SVG ELEMENT
 svg = d3.select("#d3-container")
-.append("svg")
-.attr('width', width)
-.attr('height', height)
+  .append("svg")
+  .attr('width', width)
+  .attr('height', height)
 
-svg.append("g")
-.attr("class", "xAxis")
-.attr("transform", `translate(${0}, ${height-margin.bottom})`)
+  // + CREATE AXES
+const xAxisGroup = svg.append("g")
+.attr("class", 'xAxis') //what is the differnece btween " and ' 
+.attr("transform", `translate(${0}, ${height - margin.bottom})`) 
 .call(xAxis)
-.append("text")
-.text("Ideology Score 2020")
-.attr("transform", `translate(${width/2}, ${40})`)
 
-svg.append("g")
-.attr("class", "yAxis")
-.attr("transform", `translate(${margin.left}, ${0})`)
+const yAxisGroup = svg.append("g")
+.attr("class", 'yAxis')
+.attr("transform", `translate(${margin.left}, ${0})`) 
 .call(yAxis)
 
-// SETUP UI ELEMENTS
-const dropdown = d3.select("#dropdown")
+xAxisGroup.append("text")
+  .attr("class", 'axis-title')
+  .attr("x", width / 2)
+  .attr("y", 40)
+  .attr("text-anchor", "middle")
+  .attr("fill", "black")
+  .text("Year")
 
-dropdown.selectAll("options")
-.data(["All","R", "D"])
-.join("option")
-.attr("value", d => d)
-.text(d=> d)
+yAxisGroup.append("text")
+  .attr("class", 'axis-title')
+  .attr("x", -40)
+  .attr("y", height / 2)
+  .attr("writing-mode", "vertical-lr")
+  .attr("text-anchor", "middle")
+  .attr("fill","black")
+  .text("Minimum Wage")
+
+ // + UI ELEMENT SETUP
+ const selectElement = d3.select("#dropdown")
+    
+ // + add dropdown options
+selectElement //Why did this change to selectElement from 'dropdown'?
+   .selectAll("options")
+   .data(Array.from(new Set(state.date.map(d=> d.usstate))))
+   .join("option")
+   .attr("value", d => d)
+   .text(d => d)
+
+// const dropdown = d3.select("#dropdown")
+
+// dropdown.selectAll("options")
+// .data(Array.from(new Set(state.date.map(d=> d.usstate))))
+// .join("option")
+// .attr("value", d => d)
+// .text(d => d)
+// console.log("dropdown: ", dropdown)
 
 dropdown.on("change", event=> {
-console.log("dropdown changed!", event.target.value)
-state.selectedParty = event.target.value
-console.log("new state:", state)
-draw();
+  console.log("dropdown changed!", event.target.value)
+  state.selection = event.target.value
+  console.log("new state:", state)
+  draw();
 })
 
-  draw(); // calls the draw function
+  draw();
 }
 
+// })
+draw(); // calls the draw function
+// }
+
+/* INITIALIZING FUNTION */ 
+function init(){
+
+}
 /* DRAW FUNCTION */
 // we call this everytime there is an update to the data/state
 function draw() {
+  console.log("state.selection", state.selection)
   // + FILTER DATA BASED ON STATE
+const filteredData = state.data
+.filter(d=> state.selection === d.usstate)
 
   // + UPDATE SCALE(S), if needed
 
@@ -101,4 +138,8 @@ function draw() {
   // + DEFINE LINE GENERATOR FUNCTION
 
   // + DRAW LINE AND/OR AREA
+  const lineFunction = d3.line()
+  .x(d=> xScale(d.year))
+  .y(d=> yScale(d.minwage))
+
 }
