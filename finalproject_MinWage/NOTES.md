04105f97723fe0ef555fcf5780d9f68b8bda2f2a
lauralonglastname <lauramuellersoppart@gmail.com>
2021-05-19 10:56:27 -0400

    updated graphs

--- finalproject_MinWage/NOTES.md
+++ finalproject_MinWage/NOTES.md
@@ -1,1 +1,19 @@
-Alaska did not become a state until 1959. Should I delete the $40 data points from 1938-1958 to reflect that?
+Data from 1938 to 1968 is inconsistent. For example, Alaska did not become a state until 1959. Should I delete the data points from 1938-1958 from Alaska to reflect that? Or best practives to just stick to post 1968?
+
+How do I create the fixed fed min wage line? I delete the entire area graph every time I try it out.
+
+What is happening at line 18 in html? What is the error?
+
+// /_ CREATE FIXED FED MIN WAGE LINE _/
+const lineFunction = d3.line()
+.x(d=> xScale(d.year))
+.y(d=> yScale(d.minwage))
+
+svg.selectAll("path.line")
+.data([filteredData = state.data.filter(d => state.selection === d.FedMinWage)])
+.join("path")
+.attr("class", "line")
+.attr("d", d=> lineFunction(d))
+.attr("stroke", "black")
+
+Why is the second graph MIA?
--- finalproject_MinWage/main.js
+++ finalproject_MinWage/main.js
@@ -1,3 +1,159 @@
+//MAP MODULE 
+
+/**
+ * CONSTANTS AND GLOBALS
+ * */
+const width = window.innerWidth * 0.9,
+  height = window.innerHeight * 0.7,
+  margin = { top: 50, bottom: 50, left: 60, right: 40 };
+
+/** these variables allow us to access anything we manipulate in
+ * init() but need access to in draw().
+ * All these variables are empty before we assign something to them.*/
+let svg;
+
+/**
+ * APPLICATION STATE
+ * */
+let state = {
+  // + SET UP STATE
+  geojson: null,
+  points: null,
+  hover: {
+    stateName: null,
+    Change95PercentDays: null, 
+    screenPosition: null, // will be array of [x,y] once mouse is hovered on something
+    mapPosition: null, // will be array of [long, lat] once mouse is hovered on something
+    visible: false,
+  }
+};
+
+/**
+ * LOAD DATA
+ * Using a Promise.all([]), we can load more than one dataset at a time
+ * */
+Promise.all([
+  d3.json("../data/usState.json"),
+  d3.csv("../data/MinWageMap.csv", d3.autoType),
+]).then(([geojson, pointsData]) => {
+  state.geojson = geojson
+  state.points = pointsData
+  console.log("state: ", state);
+  init();
+});
+
+/**
+ * INITIALIZING FUNCTION
+ * this will be run *one time* when the data finishes loading in
+ * */
+function init() {
+  // create an svg element in our main `d3-container` element
+  svg = d3
+    .select("#d3-map")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+    // SPECIFY PROJECTION
+    // a projection maps from lat/long -> x/y values
+    // so it works a lot like a scale!
+    const projection = d3.geoAlbersUsa()
+      .fitSize([
+      width-margin.left-margin.right,
+      height-margin.top-margin.bottom], 
+      state.geojson);
+
+    // DEFINE PATH FUNCTION
+    const path = d3.geoPath(projection)
+
+  //   const colorScale = d3.scaleSequential(d3.interpolateBlues)
+  // .domain(d3.extent(state.geojson.features, d=> d.properties.AWATER))
+
+  // + DRAW BASE MAP PATH
+      const states = svg.selectAll("path.states")
+      .data(state.geojson.features)
+      .join("path")
+      .attr("class", 'states')
+      .attr("stroke", "black")
+      .attr("fill", "#e5f5eb")
+      .attr("d", path)
+
+    // EXAPMLE #1: lat/long => x/y
+
+  svg.selectAll("circle.point")
+      .data(state.points)
+      .join("circle")
+      .attr("r", 5)
+      .attr("fill", d=> {
+        if (d.State = Greater) return "orange";
+        if (d.State = Equals) return "green";
+        if (d.State = None) return "black";
+        else return "green"
+      })
+      .attr("stroke", "gray")
+      .attr("transform", d=> {
+        const [x,y] = projection([d.Long, d.Lat])
+        return `translate(${x}, ${y})`
+      })
+
+    // EXAMPLE #2: x/y=> lat/long
+    // take mouse screen position and report location value in lat/long
+    // set up event listener on our svg to see where the mouse is
+    .on("mousemove", event => {
+      // 1. get mouse x/y position
+      const {clientX, clientY} = event
+
+      // 2. invert the projection to go from x/y => lat/long
+      // ref: https://github.com/d3/d3-geo#projection_invert
+      const [long, lat] = projection.invert([clientX, clientY])
+      state.hover=  {
+        screenPosition: [clientX, clientY], // will be array of [x,y] once mouse is hovered on something
+        mapPosition: [long, lat], // will be array of [long, lat] once mouse is hovered on something
+        visible: true
+      }
+      draw();
+    }).on("mouseout", event=>{
+      // hide tooltip when not moused over a state
+      state.hover.visible = false
+      draw(); // redraw
+    })
+
+
+
+  draw(); // calls the draw function
+}
+
+/**
+ * DRAW FUNCTION
+ * we call this everytime there is an update to the data/state
+ * */
+function draw() {
+  // add div to HTML and re-populate content every time `state.hover` updates
+  d3.select("#d3-map") // want to add
+    .selectAll('div.hover-content')
+    .data([state.hover])
+    .join("div")
+    .attr("class", 'hover-content')
+    .classed("visible", d=> d.visible)
+    .style("position", 'absolute')
+    .style("transform", d=> {
+      // only move if we have a value for screenPosition
+      if (d.screenPosition)
+      return `translate(${d.screenPosition[0]}px, ${d.screenPosition[1]}px)`
+    })
+    .html(d=> {
+      return `
+      <div>
+      State: ${d.State}</div>
+      <div>
+       2021 Minimum Wage: ${d.minwage}
+      </div>
+      `
+    })
+}
+
+// AREA GRAPH MODULE 
+
 /* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * 0.7,
   height = window.innerHeight * 0.7,
@@ -59,6 +215,8 @@ function init() {
     .attr("value", d => d)
     .text(d => d)
 
+    // How to remove just Fed from the dropdown? 
+
     dropdown.on("change", event => {
       console.log("dropdown changed!", event.target.value)
       state.selection = event.target.value
@@ -102,6 +260,18 @@ yAxisGroup.append("text")
   .attr("fill","black")
   .text("Minimum Wage $USD 2020")
 
+  // /* CREATE FIXED FED MIN WAGE LINE */ 
+const lineFunction = d3.line()
+.x(d=> xScale(d.year))
+.y(d=> yScale(d.minwage))
+
+svg.selectAll("path.line")
+.data([filteredData = state.data.filter(d => state.selection === d.FedMinWage)])
+.join("path")
+.attr("class", "line")
+.attr("d", d=> lineFunction(d))
+.attr("stroke", "blue")
+
 //  // + UI ELEMENT SETUP
 //  const selectElement = d3.select("#dropdown")
     
@@ -117,17 +287,7 @@ yAxisGroup.append("text")
 draw(); // calls the draw function
 }
 
-// /* CREATE FIXED FED MIN WAGE LINE */ 
-// const lineFunction = d3.line()
-// .x(d=> xScale(d.year))
-// .y(d=> yScale(d.minwage))
 
-// svg.selectAll("path.line")
-// .data([filteredData = state.data.filter(d => state.selection === d.FedMinWage)])
-// .join("path")
-// .attr("class", "line")
-// .attr("d", d=> lineFunction(d))
-// .attr("stroke", "black")
 
 // draw();
 // }
@@ -204,3 +364,6 @@ dots.selectAll("circle")
   .attr("d", d => areaGen(d))
 
 }
+
+// LINE GRAPH MODULE
+
