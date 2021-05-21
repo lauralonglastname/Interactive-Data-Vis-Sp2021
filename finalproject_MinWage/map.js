bbdeeff4b9e880da920830a23b3ae3164b54651f
lauralonglastname <lauramuellersoppart@gmail.com>
2021-05-21 00:28:46 -0400

    new files

--- finalproject_MinWage/map.js
+++ finalproject_MinWage/map.js
@@ -1,24 +1,22 @@
-/*** CONSTANTS AND GLOBALS
+/**
+ * CONSTANTS AND GLOBALS
  * */
-const widthMap = window.innerWidth * 0.9,
-  heightMap = window.innerHeight * 0.7,
-  marginMap = { top: 50, bottom: 50, left: 60, right: 40 };
+const width = window.innerWidth * 0.9,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
 /** these variables allow us to access anything we manipulate in
  * init() but need access to in draw().
  * All these variables are empty before we assign something to them.*/
-let svgMap;
+let svg;
 
 /**
  * APPLICATION STATE
  * */
-let stateMap = {
-  // + SET UP STATE
+let state = {
   geojson: null,
   points: null,
   hover: {
-    StateName: null, //where is this pulling from?
-    MinWage: null, 
     screenPosition: null, // will be array of [x,y] once mouse is hovered on something
     mapPosition: null, // will be array of [long, lat] once mouse is hovered on something
     visible: false,
@@ -31,54 +29,65 @@ let stateMap = {
  * */
 Promise.all([
   d3.json("../data/usState.json"),
-  d3.csv("../data/USMinWageMap.csv", d3.autoType),
+  d3.csv("../data/usHeatExtremes.csv", d3.autoType),
 ]).then(([geojson, pointsData]) => {
-  stateMap.geojson = geojson
-  stateMap.points = pointsData
-  console.log("stateMap: ", stateMap);
-  initMap();
+  state.geojson = geojson
+  state.points = pointsData
+  console.log("state: ", state);
+  init();
 });
 
 /**
  * INITIALIZING FUNCTION
  * this will be run *one time* when the data finishes loading in
  * */
-function initMap() { 
-  // create an svg element in our main `d3-map` element
-  svgMap = d3
-    .select("#d3-map")
+function init() {
+  // create an svg element in our main `d3-container` element
+  svg = d3
+    .select("#d3-container")
     .append("svg")
-    .attr("width", widthMap)
-    .attr("height", heightMap);
+    .attr("width", width)
+    .attr("height", height);
 
     // SPECIFY PROJECTION
     // a projection maps from lat/long -> x/y values
     // so it works a lot like a scale!
     const projection = d3.geoAlbersUsa()
       .fitSize([
-      widthMap-marginMap.left-marginMap.right,
-      heightMap-marginMap.top-marginMap.bottom], 
-      stateMap.geojson);
+      width-margin.left-margin.right,
+      height-margin.top-margin.bottom], state.geojson);
 
     // DEFINE PATH FUNCTION
-    const pathMap = d3.geoPath(projection)
+    const path = d3.geoPath(projection)
 
-  //   const colorScale = d3.scaleSequential(d3.interpolateBlues)
-  // .domain(d3.extent(state.geojson.features, d=> d.properties.AWATER))
-
-  // + DRAW BASE MAP PATH
-      const states = svgMap.selectAll("pathMap.states")
-      .data(stateMap.geojson.features)
-      .join("pathMap")
+    // draw base layer path - one path for each state
+    const states = svg.selectAll("path.states")
+      .data(state.geojson.features)
+      .join("path")
       .attr("class", 'states')
       .attr("stroke", "black")
-      .attr("fill", "blue")
-      //is this where i create if statements for data?
-      .attr("d", pathMap)
+      .attr("fill", "transparent")
+      .attr("d", path)
+
+    // EXAPMLE #1: lat/long => x/y
+    // draw point for CUNY graduate center
+    const gradCenterPoint =  { latitude: 40.7423, longitude: -73.9833 };
+    svg.selectAll("circle.point")
+      .data([gradCenterPoint])
+      .join("circle")
+      .attr("r", 10)
+      .attr("fill", "steelblue")
+      .attr("transform", d=> {
+        // use our projection to go from lat/long => x/y
+        // ref: https://github.com/d3/d3-geo#_projection
+        const [x, y] = projection([d.longitude, d.latitude])
+        return `translate(${x}, ${y})`
+      })
 
     // EXAMPLE #2: x/y=> lat/long
     // take mouse screen position and report location value in lat/long
     // set up event listener on our svg to see where the mouse is
+    states
     .on("mousemove", event => {
       // 1. get mouse x/y position
       const {clientX, clientY} = event
@@ -86,32 +95,32 @@ function initMap() {
       // 2. invert the projection to go from x/y => lat/long
       // ref: https://github.com/d3/d3-geo#projection_invert
       const [long, lat] = projection.invert([clientX, clientY])
-      stateMap.hover=  {
+      state.hover=  {
         screenPosition: [clientX, clientY], // will be array of [x,y] once mouse is hovered on something
         mapPosition: [long, lat], // will be array of [long, lat] once mouse is hovered on something
         visible: true
       }
-      drawMap();
+      draw();
     }).on("mouseout", event=>{
       // hide tooltip when not moused over a state
-      stateMap.hover.visible = false
-      drawMap(); // redraw
+      state.hover.visible = false
+      draw(); // redraw
     })
 
 
-
-  drawMap(); // calls the draw function
+  draw(); // calls the draw function
 }
 
 /**
  * DRAW FUNCTION
  * we call this everytime there is an update to the data/state
  * */
-function drawMap() {
+
+function draw() {
   // add div to HTML and re-populate content every time `state.hover` updates
-  d3.select("#d3-map") // want to add
+  d3.select("#d3-container") // want to add
     .selectAll('div.hover-content')
-    .data([stateMap.hover])
+    .data([state.hover])
     .join("div")
     .attr("class", 'hover-content')
     .classed("visible", d=> d.visible)
@@ -123,10 +132,9 @@ function drawMap() {
     })
     .html(d=> {
       return `
+      <div>This is a sample Tooltip</div>
       <div>
-      U.S. State: ${d.StateName}</div>
-      <div>
-      Minimum Wage: ${d.MinWage}
+      Hovered Location: ${d.mapPosition}
       </div>
       `
     })
